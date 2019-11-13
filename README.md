16812f1b52681315426d6e30c5e017fe640345d6
JavierBravoDonaire <jjbd98@hotmail.com>
2019-11-13 19:59:25 +0100

    Añado bisiestos, todavía no está terminado

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Practica_ISI_IDM
 Repositorio de la práctica de IDM.
+
+INSTRUCCIONES
+
+Cómo compilar y ejecutar los tests:
+
+cd NumZero
+javac *.java
+javac -cp ../hamcrest-core-1.3.jar:../junit-4.12.jar:. *Test.java
+javac -cp ../hamcrest-core-1.3.jar:../junit-4.12.jar:. AllTests.java
+java  -cp ../hamcrest-core-1.3.jar:../junit-4.12.jar:. AllTests
---
+++ bisiestos/Bisiestos.java
@@ -0,0 +1,11 @@
+import java.security.InvalidParameterException;
+
+public class Bisiestos {
+	// Devuelve true si año es bisiesto, false en caso contrario
+	// Eleva la excepción InvalidParameter si año no es un parámetro válido
+	
+	public boolean esBisiesto(int año) throws InvalidParameterException {
+		System.out.println("You entered: " + año);
+		return true;
+	}
+}
