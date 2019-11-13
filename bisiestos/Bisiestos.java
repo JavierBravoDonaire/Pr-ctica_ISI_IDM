6c82a949f38150e53e1dde230b5f3e24daa13321
JavierBravoDonaire <jjbd98@hotmail.com>
2019-11-13 20:16:23 +0100

    Añado .jar y mejoro bisiestosTest

--- bisiestos/Bisiestos.java
+++ bisiestos/Bisiestos.java
@@ -4,8 +4,9 @@ public class Bisiestos {
 	// Devuelve true si año es bisiesto, false en caso contrario
 	// Eleva la excepción InvalidParameter si año no es un parámetro válido
 	
-	public boolean esBisiesto(int año) throws InvalidParameterException {
-		System.out.println("You entered: " + año);
+	public static boolean esBisiesto(int año) throws InvalidParameterException {
+		if (año < 0)
+            throw new InvalidParameterException("Invalid Parameter");
 		return true;
 	}
 }
--- bisiestos/bisiestosTest.java
+++ bisiestos/bisiestosTest.java
@@ -5,5 +5,28 @@ import java.security.*;
 
 public class bisiestosTest
 {
-    ;
+   @Test (expected = InvalidParameterException.class)
+   public void testForInvalidParameter()
+   {
+        int año = -100;
+        Bisiestos.esBisiesto(año);
+   }
+   
+   @Test public void yearBisiesto()
+   {
+		int año = 2020;
+        assertEquals("Año 2020:", true, Bisiestos.esBisiesto(año));
+   }
+   
+   @Test public void yearNoBisiesto()
+   {
+		int año = 2019;
+        assertEquals("Año 2019:", false, Bisiestos.esBisiesto(año));
+   }
+   
+   @Test public void yearZero()
+   {
+		int año = 0;
+        assertEquals("Año 0:", true, Bisiestos.esBisiesto(año));
+   }
 }
