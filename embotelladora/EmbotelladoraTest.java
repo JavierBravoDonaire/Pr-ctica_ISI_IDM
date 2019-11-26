9393c081b2f37cc4d72b4a7fff66586bc6bd26e6
Adrián <adrianriao@gmail.com>
2019-11-26 19:39:27 +0100

    Añadidos ficheros testing Embotelladora

---
+++ embotelladora/EmbotelladoraTest.java
@@ -0,0 +1,64 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+import java.security.*;
+
+public class EmbotelladoraTest
+{
+   @Test (expected = InvalidParameterException.class)
+   public void testForInvalidParameter1()
+   {
+        int litros = -100;
+        int bPeq = 3;
+        int bGran = 4;
+        Embotelladora.calculaBotellasPequenas(bPeq, bGran, litros);
+   }
+   
+    @Test (expected = InvalidParameterException.class)
+   public void testForInvalidParameter2()
+   {
+        int litros = 100;
+        int bPeq = -6;
+        int bGran = 4;
+        Embotelladora.calculaBotellasPequenas(bPeq, bGran, litros);
+   }
+   
+    @Test (expected = InvalidParameterException.class)
+   public void testForInvalidParameter3()
+   {
+        int litros = 100;
+        int bPeq = 3;
+        int bGran = -4;
+        Embotelladora.calculaBotellasPequenas(bPeq, bGran, litros);
+   }
+   
+    // NoSolution: bPeq < litros - 5*bGran
+    @Test (expected = NoSolution.class)
+   public void testNoSolution()
+   {
+		int litros = 100;
+        int bPeq = 3;
+        int bGran = 4;
+        Embotelladora.calculaBotellasPequenas(bPeq, bGran, litros));
+   }
+   
+   // IgualCapacidad: bPeq = litros - 5*bGran
+   @Test public void testIgualCapacidad()
+   {
+		int litros = 30;
+        int bPeq = 5;
+        int bGran = 5;
+        assertEquals("igual Capacidad:", litros - 5*bGran,
+                     Embotelladora.calculaBotellasPequenas(bPeq, bGran, litros));
+   }
+   
+   // Mayor Capacidad: bPeq > litros - 5*bGran
+   @Test public void testMayorCapacidad()
+   {
+		int litros = 30;
+        int bPeq = 34;
+        int bGran = 5;
+        assertEquals("Mayor Capacidad:", litros - 5*bGran,
+                     Embotelladora.calculaBotellasPequenas(bPeq, bGran, litros));
+   }
+}
